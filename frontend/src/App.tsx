2e466b3d7c00115a287b999aa6dc122b0474b235
saharacode <johanndallmann@gmx.de>
2023-06-09 11:26:03 +0200

    logo added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import HelloWorldComponent from "./components/helloWorldComponent/HelloWorldComponent";
+import UrbanPilotLogo from './images/UrbanPilotLogo.png';
 
 function App() {
   return (
     <div className="App">
         <div>
+            <img src={UrbanPilotLogo} alt={"UrbanPilotLogo"} style={{width: '10%'}}/>
+            <h3>Discover. Share. Conquer the urban jungle.</h3>
             <HelloWorldComponent/>
         </div>
 
