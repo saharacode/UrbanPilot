85b860abc5a719dd86e1dde5efe26b0ea7f3036b
saharacode <johanndallmann@gmx.de>
2023-06-13 16:44:31 +0200

    endpoint for logout added

--- backend/src/main/java/de/neuefische/backend/controller/UserController.java
+++ backend/src/main/java/de/neuefische/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package de.neuefische.backend.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +19,10 @@ public class UserController {
                 .getName();
     }
 
-    @GetMapping("/login2")
-    public String login2(){
-        System.out.println(SecurityContextHolder.getContext());
-        return SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-                .getName();
+    @PostMapping("/logout")
+    String logout(HttpSession httpSession) {
+        httpSession.invalidate();
+        SecurityContextHolder.clearContext();
+        return "logged out";
     }
 }
