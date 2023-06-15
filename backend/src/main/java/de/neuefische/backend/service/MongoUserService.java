3d6a827ae19efdec1cf211160ba477b031f329ec
saharacode <johanndallmann@gmx.de>
2023-06-15 13:27:48 +0200

    password-hashing added

--- backend/src/main/java/de/neuefische/backend/service/MongoUserService.java
+++ backend/src/main/java/de/neuefische/backend/service/MongoUserService.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.List;
 public class MongoUserService implements UserDetailsService {
     private final MongoUserRepo mongoUserRepo;
     private final GenerateUUIDService generateUUIDService;
+    PasswordEncoder encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         MongoUser mongoUser = mongoUserRepo.findMongoUserByUsername(username)
@@ -24,7 +27,9 @@ public class MongoUserService implements UserDetailsService {
     }
 
     public MongoUser registerUser(MongoUser newUserWithoutId) {
-        MongoUser newUser = new MongoUser(generateUUIDService.generateUUID(),newUserWithoutId.getUsername(), newUserWithoutId.getPassword(), newUserWithoutId.getFullname(), newUserWithoutId.getEmail(), newUserWithoutId.getHomecity());
+        String newUUID = generateUUIDService.generateUUID();
+        String hashedPassword = encoder.encode(newUserWithoutId.getPassword());
+        MongoUser newUser = new MongoUser(newUUID,newUserWithoutId.getUsername(), hashedPassword, newUserWithoutId.getFullname(), newUserWithoutId.getEmail(), newUserWithoutId.getHomecity());
 
         return mongoUserRepo.save(newUser);
     }
