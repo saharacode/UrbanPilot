2e820236919e29a3b700ee67f99c44467b6262ea
saharacode <johanndallmann@gmx.de>
2023-06-21 18:09:17 +0200

    unneccessary imports removed

--- backend/src/test/java/de/neuefische/backend/service/MongoUserServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/MongoUserServiceTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.HashMap;
