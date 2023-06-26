c64bda01dc428b000e5a3c91c0788ed06b448498
saharacode <johanndallmann@gmx.de>
2023-06-26 17:08:13 +0200

    check if homecity is in db

--- backend/src/main/java/de/neuefische/backend/repository/CitiesRepo.java
+++ backend/src/main/java/de/neuefische/backend/repository/CitiesRepo.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface CitiesRepo extends MongoRepository<UserCity,String> {
-    Optional<UserCity> findUserCityByCityId(String cityId);
+    Optional<UserCity> findUserCityByCityName(String cityname);
 }
--- backend/src/main/java/de/neuefische/backend/service/user/GenerateUserCityCollectionService.java
+++ backend/src/main/java/de/neuefische/backend/service/user/GenerateUserCityCollectionService.java
@@ -3,6 +3,7 @@ package de.neuefische.backend.service.user;
 import de.neuefische.backend.model.cityCollection.UserCity;
 import de.neuefische.backend.model.cityCollection.UserCityCollection;
 import de.neuefische.backend.model.user.ImportMongoUserDTO;
+import de.neuefische.backend.repository.CitiesRepo;
 import de.neuefische.backend.repository.CityCollectionRepo;
 import de.neuefische.backend.service.GenerateUUIDService;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,14 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class GenerateUserCityCollectionService {
     private final CityCollectionRepo cityCollectionRepo;
+    private final CitiesRepo citiesRepo;
     private final GenerateUUIDService generateUUIDService;
 
     public String generateUserCityCollection(ImportMongoUserDTO newUserWithoutId){
         String newUUID = generateUUIDService.generateUUID();
-        UserCity defaultUserCity = new UserCity("testCityId", newUserWithoutId.getHomecity(), "testCityCoordinates");
+        String newUUIDforUnknownCity = generateUUIDService.generateUUID();
+        UserCity defaultUserCity = citiesRepo.findUserCityByCityName(newUserWithoutId.getHomecity())
+                .orElse(new UserCity(newUUIDforUnknownCity, "unknown City", "unknownCoordinates"));
         Map<String, UserCity> newUserCityMap = new HashMap<>();
         newUserCityMap.put(defaultUserCity.getCityId(), defaultUserCity);
         UserCityCollection newUserCityCollection =new UserCityCollection(newUUID,newUserCityMap);
