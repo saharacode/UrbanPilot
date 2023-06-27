0b42c06f397b93a405e029d89ec45909fa1dd036
saharacode <johanndallmann@gmx.de>
2023-06-27 18:23:45 +0200

    input for AddLocationPopUp added

--- frontend/src/components/Mainpage/AddLocationPopUp/AddLocationPopUp.tsx
+++ frontend/src/components/Mainpage/AddLocationPopUp/AddLocationPopUp.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import Popup from 'reactjs-popup';
 import './Popup.css';
+import {LocationInfo} from "../../../model/LocationInfo";
 
-type Props = {
-}
+function AddLocationPopUp() {
+    const [locationName, setLocationName] = useState<string>("");
+    const [locationCity, setLocationCity] = useState<string>("");
+    const [locationDescription, setLocationDescription] = useState<string>("");
+    const [latCoordinate, setLatCoordinate] = useState<number>(0);
+    const [lngCoordinate, setLngCoordinate] = useState<number>(0);
+    const [locationType, setLocationType] = useState<string>("");
 
-function AddLocationPopUp(props:Props) {
+    function addLocationInputHandler(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const newLocation: LocationInfo = {
+            locationId: "",
+            locationName: locationName,
+            locationCity: locationCity,
+            locationDescription: locationDescription,
+            locationLatCoordinate: latCoordinate,
+            locationLngCoordinate: lngCoordinate,
+            locationType: locationType
+        };
+        console.log(newLocation);
+    }
+
     return (
         <div>
             <Popup trigger={<button>Add location</button>}>
                 <div className="popupStyle">
                     <h3>Add a new Location</h3>
-                    <div>
-                        <form>
-                            <div>
+                    <div className="registerFormContainer">
+                        <form onSubmit={addLocationInputHandler} className="registerForm">
+                            <div className="inputAndLabel">
                                 <label>Name:</label>
-                                <input type={"text"} />
+                                <input type={"text"} onChange={event => setLocationName(event.target.value)}/>
                             </div>
-                            <div>
+                            <div className="inputAndLabel">
                                 <label>Description:</label>
-                                <input type={"text"}/>
+                                <input type={"text"} onChange={event => setLocationDescription(event.target.value)}/>
                             </div>
-                            <button type={"submit"}>Add location</button>
+                            <div className="inputAndLabel">
+                                <label>Type:</label>
+                                <input type={"text"} onChange={event => setLocationType(event.target.value)}/>
+                            </div>
+                            <div className="inputAndLabel">
+                                <label>City:</label>
+                                <input type={"text"} onChange={event => setLocationCity(event.target.value)}/>
+                            </div>
+                            <div className="inputAndLabel">
+                                <label>Lat Coordinate:</label>
+                                <input type={"number"} onChange={event => setLatCoordinate(parseFloat(event.target.value))}/>
+                            </div>
+                            <div className="inputAndLabel">
+                                <label>Lng Coordinate:</label>
+                                <input type={"number"} onChange={event => setLngCoordinate(parseFloat(event.target.value))}/>
+                            </div>
+                            <button type={"submit"}>Add</button>
                         </form>
                     </div>
                 </div>
