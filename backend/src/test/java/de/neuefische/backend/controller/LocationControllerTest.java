a1e80e18dd5dab8e8b62f829915525b066b445ec
saharacode <johanndallmann@gmx.de>
2023-06-29 09:18:59 +0200

    integrationtest getalllocations with list with one location added

--- backend/src/test/java/de/neuefische/backend/controller/LocationControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/LocationControllerTest.java
@@ -55,6 +55,73 @@ class LocationControllerTest {
     @Test
     @DirtiesContext
     @WithMockUser(username = "testuser", password = "testpassword")
+    void getAllLocationsForUser_thenReturnStatus200_andListWithOneLocation() throws Exception {
+        ImportMongoUserDTO newUserWithoutId = ImportMongoUserDTO.builder()
+                .username("testuser")
+                .fullname("testuser")
+                .password("testpassword")
+                .email("test@mail.de")
+                .homecity("Berlin")
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRequestBody = objectMapper.writeValueAsString(newUserWithoutId);
+
+        mvc.perform(MockMvcRequestBuilders.post("/user/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody)
+                        .with(csrf()))
+                .andExpect(status().isCreated());
+
+        ImportLocationDTO testImportLocationDTO = ImportLocationDTO.builder()
+                .locationName("TestName")
+                .locationCity("TestCity")
+                .locationDescription("TestDescription")
+                .locationLatCoordinate(0.0)
+                .locationLngCoordinate(0.0)
+                .locationType("TestLocationType")
+                .build();
+        String jsonRequestBodyLocation = objectMapper.writeValueAsString(testImportLocationDTO);
+
+        mvc.perform(MockMvcRequestBuilders.post("/locations/add/testuser")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBodyLocation)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            {
+                                  "locationName": "TestName",
+                                  "locationCity": "TestCity",
+                                  "locationDescription": "TestDescription",
+                                  "locationLatCoordinate": 0.0,
+                                  "locationLngCoordinate": 0.0,
+                                  "locationType": "TestLocationType"
+                            }
+                        """))
+                .andExpect(jsonPath("$.locationId").isNotEmpty());
+
+
+        mvc.perform(MockMvcRequestBuilders.get("/locations/all/testuser")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            [
+                                {
+                                      "locationName": "TestName",
+                                      "locationCity": "TestCity",
+                                      "locationDescription": "TestDescription",
+                                      "locationLatCoordinate": 0.0,
+                                      "locationLngCoordinate": 0.0,
+                                      "locationType": "TestLocationType"
+                                }
+                            ]
+                        """))
+                .andExpect(jsonPath("$[0].locationId").isNotEmpty());
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "testuser", password = "testpassword")
     void addLocation_thenReturnStatus200_andAddedLocation() throws Exception {
         ImportMongoUserDTO newUserWithoutId = ImportMongoUserDTO.builder()
                 .username("testuser")
