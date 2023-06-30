a38cef4f06fbf873beae2c925024cbc91b2cd31d
saharacode <johanndallmann@gmx.de>
2023-06-30 13:10:44 +0200

    form with inputs added

--- frontend/src/components/Mainpage/Mapcomponent/Location/EditLocationPopUp.tsx
+++ frontend/src/components/Mainpage/Mapcomponent/Location/EditLocationPopUp.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Popup from "reactjs-popup";
+import {Field, Form, Formik} from "formik";
 
 type Props = {}
 
@@ -8,7 +9,51 @@ function EditLocationPopUp(props:Props) {
         <div>
             <Popup trigger={<button>Edit</button>}>
                 <div className="popupStyle">
-                    <h3>testedit</h3>
+                    <h3>Edit Location</h3>
+                    <div className="registerFormContainer">
+                        <Formik
+                            initialValues={{
+                                locationName: "",
+                                locationDescription: "testdescription",
+                                locationType: "",
+                                locationCity: "",
+                                locationLatCoordinate: "",
+                                locationLngCoordinate: ""
+                            }}
+                            onSubmit={async (values) => {
+                                await new Promise((resolve) => setTimeout(resolve, 500));
+                                alert(JSON.stringify(values, null, 2));
+                            }}
+                        >
+                            <Form>
+                                <div className="inputAndLabel">
+                                    <label>Name:</label>
+                                    <Field name="locationName" type="text"/>
+                                </div>
+                                <div className="inputAndLabel">
+                                    <label>Description:</label>
+                                    <Field name="locationDescription" type="text"/>
+                                </div>
+                                <div className="inputAndLabel">
+                                    <label>Type:</label>
+                                    <Field name="locationType" type="text"/>
+                                </div>
+                                <div className="inputAndLabel">
+                                    <label>City:</label>
+                                    <Field name="locationCity" type="text"/>
+                                </div>
+                                <div className="inputAndLabel">
+                                    <label>Lat Coordinate:</label>
+                                    <Field name="locationLatCoordinate" type="text"/>
+                                </div>
+                                <div className="inputAndLabel">
+                                    <label>Lng Coordinate:</label>
+                                    <Field name="locationLngCoordinate" type="text"/>
+                                </div>
+                                <button type="submit">Save</button>
+                            </Form>
+                        </Formik>
+                    </div>
                 </div>
             </Popup>
         </div>
