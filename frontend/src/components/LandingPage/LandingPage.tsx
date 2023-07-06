125837991bf774c6a00cb19fa18f84fa7320ed97
saharacode <johanndallmann@gmx.de>
2023-07-06 18:25:39 +0200

    css classes added

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import "./LandingPage.css"
+import UrbanPilotLogo from "../../images/UrbanPilotLogo.png";
+import {LogoutIcon} from "../../icons/logout-icon";
 
 function LandingPage() {
     const nav = useNavigate();
@@ -9,12 +12,14 @@ function LandingPage() {
     }
 
     return (
-        <div>
-            <div>
-                <h1>Welcome to UrbanPilot!</h1>
-                <button onClick={goTologinButtonHandler}>Go to login</button>
-            </div>
+        <div className="landingpage-container">
+            <img className="landingpage-logo" src={UrbanPilotLogo} alt={"UrbanPilotLogo"}/>
+            <button className="continueToLogin-btn" onClick={goTologinButtonHandler}>
+                <p>Go to login</p>
+                <LogoutIcon width={30} height={30} color={"white"}/>
+            </button>
         </div>
+
     );
 }
 
