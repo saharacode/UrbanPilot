b6c415001225c916944214f679048e26d65be1d1
saharacode <johanndallmann@gmx.de>
2023-07-07 10:19:52 +0200

    loginpage style added

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -2,6 +2,7 @@ import React, {FormEvent, useState} from 'react';
 import UrbanPilotLogo from "../../images/UrbanPilotLogo.png";
 import './Login.css';
 import {useNavigate} from "react-router-dom";
+import {LoginIcon} from "../../icons/login-icon";
 
 type Props = {
     postLogin: (username:string,password:string) => Promise<void>;
@@ -26,29 +27,26 @@ function Login(props:Props) {
     }
 
     return (
-        <div>
-            <div className={"landingpage-container"}>
-                <img className="landingpage-logo" src={UrbanPilotLogo} alt={"UrbanPilotLogo"}/>
-                <h3>Discover. Share. Conquer the urban jungle.</h3>
-            </div>
-            <div>
-                <h1>Please login:</h1>
-            </div>
-            <div>
+        <div className={"landingpage-container"}>
+            <img className="landingpage-logo" src={UrbanPilotLogo} alt={"UrbanPilotLogo"}/>
+            <h3>Discover. Share. Conquer the urban jungle.</h3>
+            <div className="login-container">
+                <h2>Please login:</h2>
                 {props.userExists ? <></> : <h5>{props.errormessage}</h5>}
-            </div>
-            <div>
                 <form onSubmit={loginInputHandler} className="loginForm">
                     <input type={"text"} placeholder={"Username..."} onChange={event => setUsername(event.target.value)}/>
                     <input type={"password"} placeholder={"Password..."} onChange={event => setPassword(event.target.value)}/>
-                    <button type={"submit"}>Login</button>
+                    <button className="login-btn" type={"submit"}>
+                        <LoginIcon width={30} height={30} color={"white"}/>
+                    </button>
                 </form>
             </div>
-            <div>
+            <div className="register-container">
                 <h4>New here?</h4>
                 <button onClick={goToSignUpButtonHandler}>Sign up</button>
             </div>
         </div>
+
     );
 }
 
