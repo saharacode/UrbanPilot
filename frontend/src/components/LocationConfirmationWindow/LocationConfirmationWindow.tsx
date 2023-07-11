d02cd68198dc000b608554c97212664327e23363
saharacode <johanndallmann@gmx.de>
2023-07-11 20:31:40 +0200

    coordinates-icon added to confirmation window

--- frontend/src/components/LocationConfirmationWindow/LocationConfirmationWindow.tsx
+++ frontend/src/components/LocationConfirmationWindow/LocationConfirmationWindow.tsx
@@ -2,6 +2,7 @@ import React, {Dispatch, SetStateAction} from 'react';
 import "./LocationConfirmationWindow.css";
 import {SaveIcon} from "../../icons/save-icon";
 import {CloseIcon} from "../../icons/close-icon";
+import {CoordinatesIcon} from "../../icons/coordinates-icon";
 
 type Props={
     newLocationCoordinates: { lat: number; lng: number; };
@@ -25,12 +26,15 @@ function LocationConfirmationWindow(props:Props) {
     return (
         <div className="locationConfirmationWindow-container">
             <h3>Confirm Location</h3>
-            <div className={"locationConfirmationWindow-content"}>
-                <h5>Would you like to create a new Location with the chosen coordinates?</h5>
-                <div>
-                    <h5>Lat {props.newLocationCoordinates.lat}</h5>
-                    <h5>Lng {props.newLocationCoordinates.lng}</h5>
+            <div className="locationConfirmationWindow-content">
+                <div className="coordinates-container">
+                    <CoordinatesIcon width={20} height={20} color={"white"}/>
+                    <div>
+                        <h5>Lat: {props.newLocationCoordinates.lat}</h5>
+                        <h5>Lng: {props.newLocationCoordinates.lng}</h5>
+                    </div>
                 </div>
+                <h5>Would you like to create a new Location with the chosen coordinates?</h5>
                 <div className={"locationConfirmation-buttons"}>
                     <button className={"icon-btn"} onClick={confirmButtonHandler}>
                         <SaveIcon width={30} height={30} color={"green"}/>
